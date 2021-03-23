5590f190c14023c74a973f4183131b5870111f7f
Amandaoliveira18 <amanda-santos1998@hotmail.com>
2021-03-23 09:12:14 -0300

    Atividade inicial

---
+++ atividade/atividade/Program.cs
@@ -0,0 +1,19 @@
+﻿using AtividadeFundamentos;
+using System;
+
+namespace atividade
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Soma soma1 = new Soma();
+            soma1.Conta();
+            Soma soma2 = new Soma();
+            Console.WriteLine(soma2.SomaOpcionalNumeros(5,20,50,100)); 
+
+
+        }
+    }
+}
---
+++ atividade/atividade/Soma.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtividadeFundamentos
+{
+    public class Soma
+    {
+        public int Numero1 { get; set; }
+        public int Numero2 { get; set; }
+
+        public Soma(int numero1, int numero2)
+        {
+            Numero1 = numero1;
+            Numero2 = numero2;
+        }
+        public Soma()
+        {
+            
+        }
+
+        public int Conta(int numero1, int numero2)
+        {
+            return (numero1 + numero2);
+        }
+
+        public void Conta()
+        {
+            Console.WriteLine("Nenhum número informado");
+        }
+       
+
+        public int SomaOpcionalNumeros(int x, int y=20,int z=30,int w = 10)
+        {
+            return x + y + z + w;
+        }
+    }
+}
