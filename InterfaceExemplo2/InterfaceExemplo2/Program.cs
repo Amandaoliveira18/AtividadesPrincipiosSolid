4eeadd3bde0c8894e3914568857a0c951fe11ceb
Amandaoliveira18 <amanda-santos1998@hotmail.com>
2021-04-01 20:59:43 -0300

    Interface exemplo

---
+++ InterfaceExemplo2/InterfaceExemplo2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceExemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
